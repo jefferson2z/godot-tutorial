5a6bec3bb721ca5b46cd178dc21e285de17a2436
jefferson <jefferson.dev.insights@gmail.com>
2020-04-08 22:55:48 -0300

    finished working game

--- Main.gd
+++ Main.gd
@@ -19,11 +19,18 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUB.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
-func game_start():
+
+func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUB.update_score(score)
+	$HUB.show_message('Get Ready')
+	$Music.play()
 
 
 func _on_StartTimer_timeout():
@@ -32,6 +39,7 @@ func _on_StartTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUB.update_score(score)
 
 
 func _on_MobTimer_timeout():
@@ -44,4 +52,5 @@ func _on_MobTimer_timeout():
 	mob.rotation = direction
 	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
 	mob.linear_velocity = mob.linear_velocity.rotated(direction)
-	
+	$HUB.connect("start_game", mob, "_on_start_game")
+
--- Mob.gd
+++ Mob.gd
@@ -13,6 +13,8 @@ var mob_types = ["walk", "swim", "fly"]
 func _ready():
 	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
 
+func _on_start_game():
+	queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
